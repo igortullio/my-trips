0c6b7bb2d2c56966daaaa22ff86cf10fccaee615
Igor Tullio <igortullio@hotmail.com>
2021-03-11 15:58:32 -0300

    Make the marker dynamic

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,16 +1,29 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-const Map = () => (
+export type MapProps = {
+  places?: Place[]
+}
+
+type Place = {
+  id: string
+  name: string
+  slug: string
+  location: {
+    latitude: number
+    longitude: number
+  }
+}
+
+const Map = ({ places }: MapProps) => (
   <MapContainer center={[0, 0]} zoom={3} style={{ height: '100%', width: '100%' }}>
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[0, 0]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+
+    {places?.map(({ id, name, location }) => (
+      <Marker key={`place-${id}`} title={name} position={[location.latitude, location.longitude]} />
+    ))}
   </MapContainer>
 )
 
